af127587a624f006acf19a669b1dda9f456f14e7
Ashish Sangale <aashishsangale07@gmail.com>
2019-09-29 21:25:05 +0530

    integrating cometchat & other components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { API_ID } from "./setting.js";
+import { CometChat } from "@cometchat-pro/chat";
+import Chatbox from "./components/Chatbox";
+import ChatHome from "./components/Dashboard";
+import Login from "./components/Login";
+import CreateGroup from "./components/CreateGroup";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    CometChat.init(API_ID).then(
+      hasInitialized => {
+        console.log("Initialization completed successfully", hasInitialized);
+      },
+      error => {
+        console.log("Initialization failed with error:", error);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/chat" component={Chatbox} />
+            <Route path="/dashboard" component={ChatHome} />
+            <Route path="/creategroup" component={CreateGroup} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
